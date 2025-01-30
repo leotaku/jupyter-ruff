94c782ca6eb1e279f1ba4ae03fb83fc56427751c
Leo Gaskin <leo.gaskin@le0.gs>
2025-01-30 04:13:18 +0100

    Add config discovery

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,57 @@ import {
 
 import { ReadonlyPartialJSONObject } from '@lumino/coreutils';
 import { ICommandPalette } from '@jupyterlab/apputils';
-import { INotebookTools, NotebookActions } from '@jupyterlab/notebook';
+import {
+  INotebookTools,
+  NotebookActions,
+  NotebookPanel
+} from '@jupyterlab/notebook';
 import { ICellModel } from '@jupyterlab/cells';
+import { PathExt } from '@jupyterlab/coreutils';
+
 import init, { Workspace } from '@astral-sh/ruff-wasm-web';
+import * as toml from 'smol-toml';
 
 function canBeFormatted(cellModel: ICellModel | undefined): boolean {
   return cellModel?.type === 'code' && cellModel?.mimeType === 'text/x-ipython';
+}
+
+function format(workspace: Workspace, text: string): string {
+  return workspace.format(text).trimEnd();
+}
+
+async function workspaceFromEnvironment(
+  app: JupyterFrontEnd,
+  notebook: NotebookPanel
+): Promise<Workspace> {
+  let directory = notebook.context.path;
+  do {
+    directory = PathExt.dirname(directory);
+
+    for (const filename of ['.ruff.toml', 'ruff.toml', 'pyproject.toml']) {
+      const contents = await app.serviceManager.contents.get(
+        PathExt.join(directory, filename)
+      );
+      if (!contents) continue;
+
+      const config = toml.parse(contents.content);
+      if (filename === 'pyproject.toml') {
+        const ruffSection = configRuffSection(config);
+        if (ruffSection !== undefined) return new Workspace(config);
+      } else {
+        return new Workspace(config);
+      }
+    }
+  } while (directory !== '');
+
+  return new Workspace(Workspace.defaultSettings());
+}
+
+function configRuffSection(
+  config: Record<string, toml.TomlPrimitive>
+): toml.TomlPrimitive | undefined {
+  if (!(config['tool'] instanceof Object)) return false;
+  return (config['tool'] as Record<string, toml.TomlPrimitive>)['ruff'];
 }
 
 /**
@@ -28,25 +73,19 @@ const plugin: JupyterFrontEndPlugin<void> = {
   ) => {
     await init();
 
-    const workspace = new Workspace({
-      'line-length': 88,
-      'indent-width': 4,
-      format: {
-        'indent-style': 'space',
-        'quote-style': 'double'
-      }
-    });
-
-    function format(text: string): string {
-      return workspace.format(text).trimEnd();
+    async function getWorkspace(): Promise<Workspace> {
+      return workspaceFromEnvironment(app, tools.activeNotebookPanel!);
     }
 
     app.commands.addCommand('jupyter-ruff:format-cell', {
       label: 'Format cell using Ruff',
       isEnabled: () => canBeFormatted(tools.activeCell?.model),
       isVisible: () => true,
-      execute: function (_args: ReadonlyPartialJSONObject) {
-        const formatted = format(tools.activeCell?.model.sharedModel.source!);
+      execute: async function (_args: ReadonlyPartialJSONObject) {
+        const formatted = format(
+          await getWorkspace(),
+          tools.activeCell?.model.sharedModel.source!
+        );
         tools.activeCell?.model.sharedModel.setSource(formatted);
       }
     });
@@ -55,23 +94,29 @@ const plugin: JupyterFrontEndPlugin<void> = {
       label: 'Format all cells using Ruff',
       isEnabled: () => true,
       isVisible: () => true,
-      execute: function (_args: ReadonlyPartialJSONObject) {
+      execute: async function (_args: ReadonlyPartialJSONObject) {
         const cells = tools.activeNotebookPanel?.content.model?.cells || [];
         for (const cell of cells) {
           if (!canBeFormatted(cell)) continue;
 
-          const formatted = format(cell.sharedModel.source!);
+          const formatted = format(
+            await getWorkspace(),
+            cell.sharedModel.source!
+          );
           cell.sharedModel.setSource(formatted);
         }
       }
     });
 
     let autoFormatToggle = false;
-    NotebookActions.executionScheduled.connect((_, { cell }) => {
+    NotebookActions.executionScheduled.connect(async (_, { cell }) => {
       if (!autoFormatToggle) return;
       if (!canBeFormatted(cell.model)) return;
 
-      const formatted = format(cell.model.sharedModel.source!);
+      const formatted = format(
+        await getWorkspace(),
+        cell.model.sharedModel.source!
+      );
       cell.model.sharedModel.setSource(formatted);
     });
 
