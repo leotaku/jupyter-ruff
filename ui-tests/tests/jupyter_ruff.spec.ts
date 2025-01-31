f74f268b3fa1cd6eee869f582f3ed9f1bd306989
Leo Gaskin <leo.gaskin@le0.gs>
2025-01-31 10:35:02 +0100

    Add integration test for respecting config files

--- ui-tests/tests/jupyter_ruff.spec.ts
+++ ui-tests/tests/jupyter_ruff.spec.ts
@@ -1,4 +1,5 @@
 import { expect, test } from '@jupyterlab/galata';
+import * as path from 'path';
 
 test('should format the only existing cell', async ({ page }) => {
   await page.notebook.createNew('Test.ipynb', { kernel: 'python3' });
@@ -8,4 +9,30 @@ test('should format the only existing cell', async ({ page }) => {
   });
 
   expect(await page.notebook.getCellTextInput(0)).toBe(`a = 1 + 1`);
+});
+
+const fourIndentedCode = `
+def nothing():
+    pass
+`.trim();
+
+const twoIndentedCode = `
+def nothing():
+  pass
+`.trim();
+
+test('should respect configuration files', async ({ page, tmpPath }) => {
+  page.contents.uploadContent(
+    `indent-width = 2`,
+    'text',
+    path.join(tmpPath, 'ruff.toml')
+  );
+
+  await page.notebook.createNew('Test.ipynb', { kernel: 'python3' });
+  await page.notebook.setCell(0, 'code', fourIndentedCode);
+  await page.evaluate(async () => {
+    await window.jupyterapp.commands.execute('jupyter-ruff:format-cell');
+  });
+
+  expect(await page.notebook.getCellTextInput(0)).toBe(twoIndentedCode);
 });
