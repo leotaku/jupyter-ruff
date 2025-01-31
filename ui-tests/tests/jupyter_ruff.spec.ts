b48a391097a7fb4ab7fcb0bde798549eeed8e15f
Leo Gaskin <leo.gaskin@le0.gs>
2025-01-31 10:34:41 +0100

    Use backquotes for "code" strings

--- ui-tests/tests/jupyter_ruff.spec.ts
+++ ui-tests/tests/jupyter_ruff.spec.ts
@@ -2,10 +2,10 @@ import { expect, test } from '@jupyterlab/galata';
 
 test('should format the only existing cell', async ({ page }) => {
   await page.notebook.createNew('Test.ipynb', { kernel: 'python3' });
-  await page.notebook.setCell(0, 'code', 'a  =  1+1');
+  await page.notebook.setCell(0, 'code', `a  =  1+1`);
   await page.evaluate(async () => {
     await window.jupyterapp.commands.execute('jupyter-ruff:format-cell');
   });
 
-  expect(await page.notebook.getCellTextInput(0)).toBe('a = 1 + 1');
+  expect(await page.notebook.getCellTextInput(0)).toBe(`a = 1 + 1`);
 });
