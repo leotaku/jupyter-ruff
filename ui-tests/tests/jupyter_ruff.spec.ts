3e28949844a0e3730670387f9772b2043ecfd136
Leo Gaskin <leo.gaskin@le0.gs>
2025-02-01 14:33:03 +0100

    Add more tests

--- ui-tests/tests/jupyter_ruff.spec.ts
+++ ui-tests/tests/jupyter_ruff.spec.ts
@@ -20,6 +20,19 @@ const test = base.extend<{
 
 test.use({ notebooksDirectory: path.resolve(__dirname, '../specs') });
 
+test('should format all cells', async ({ notebook }) => {
+  await notebook.open('AllCells.ipynb');
+  await notebook.activate('AllCells.ipynb');
+
+  await notebook.page.evaluate(async () => {
+    await window.jupyterapp.commands.execute('jupyter-ruff:format-all-cells');
+  });
+
+  expect(await notebook.getCellTextInput(0)).toBe(
+    await notebook.getCellTextInput(1)
+  );
+});
+
 test('should format the cell', async ({ notebook }) => {
   await notebook.open('Simple.ipynb');
   await notebook.selectCells(0);
@@ -41,6 +54,38 @@ test('should format the cell with config', async ({ notebook, tmpPath }) => {
   );
 
   await notebook.open('WithConfig.ipynb');
+  await notebook.selectCells(0);
+
+  await notebook.page.evaluate(async () => {
+    await window.jupyterapp.commands.execute('jupyter-ruff:format-cell');
+  });
+
+  expect(await notebook.getCellTextInput(0)).toBe(
+    await notebook.getCellTextInput(1)
+  );
+});
+
+test('should isort the cell', async ({ notebook }) => {
+  await notebook.open('Isort.ipynb');
+  await notebook.selectCells(0);
+
+  await notebook.page.evaluate(async () => {
+    await window.jupyterapp.commands.execute('jupyter-ruff:format-cell');
+  });
+
+  expect(await notebook.getCellTextInput(0)).toBe(
+    await notebook.getCellTextInput(1)
+  );
+});
+
+test('should isort the cell with config', async ({ notebook, tmpPath }) => {
+  notebook.contents.uploadContent(
+    `[lint.isort]\nfrom-first = true`,
+    'text',
+    path.join(tmpPath, 'ruff.toml')
+  );
+
+  await notebook.open('IsortWithConfig.ipynb');
   await notebook.selectCells(0);
 
   await notebook.page.evaluate(async () => {
